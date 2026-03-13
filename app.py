84e708de8b2b222017a2e60fb0c2d3370fcafa4a
CADCAMBRASIL <brendo@cadcambrasil.com>
2026-03-13 01:38:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def gemini():
 
     url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={API_KEY}"
 
-
+ 
     payload = {
         "contents": [
             {"role": "user", "parts": [{"text": prompt}]}
