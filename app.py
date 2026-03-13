f60e0afa4f9d18af12dcde659a6c30034e9ffd41
CADCAMBRASIL <brendo@cadcambrasil.com>
2026-03-13 00:52:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def gemini():
     if not prompt:
         return jsonify({"text": "⚠️ Prompt vazio recebido."})
 
-    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key={API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+
 
     payload = {
         "contents": [
